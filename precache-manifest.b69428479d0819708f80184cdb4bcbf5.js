09bfd8bbc5be61a3cf7d40339aa0a78454cc9cc7
AlexCarlile <114814856+AlexCarlile@users.noreply.github.com>
2024-03-11 12:56:49 +0300

    Убран раздел статей в итоговом билде

---
+++ precache-manifest.b69428479d0819708f80184cdb4bcbf5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b5ff81c9d51d0ea7ee9a4028092c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09981a60e9728bd523f",
+    "url": "/static/css/main.e4cf5aa6.chunk.css"
+  },
+  {
+    "revision": "2e3786c23418a7bc7699",
+    "url": "/static/js/2.d74dbfc9.chunk.js"
+  },
+  {
+    "revision": "0f431788df9fedcd7536120fa5bb2b3b",
+    "url": "/static/js/2.d74dbfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09981a60e9728bd523f",
+    "url": "/static/js/main.4f81f881.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "a096107e9fcf2910c7c2e28eb418d502",
+    "url": "/static/media/Clinic.a096107e.jpg"
+  },
+  {
+    "revision": "a44dbfda974fcd472e70bf50c99b7b00",
+    "url": "/static/media/Fadeev.a44dbfda.png"
+  },
+  {
+    "revision": "c2dbf7e45352e236bb4f09ebb53b1254",
+    "url": "/static/media/Fadeev.c2dbf7e4.jpg"
+  },
+  {
+    "revision": "4177b34b74fb4a05721f1908893cbd41",
+    "url": "/static/media/Group.4177b34b.png"
+  },
+  {
+    "revision": "f4e652b9c6e12a5b61c0cd1d03c73ae3",
+    "url": "/static/media/Page-title_bckg.f4e652b9.svg"
+  },
+  {
+    "revision": "692fe913e73d5ede1e55dd7f8fa6ef8b",
+    "url": "/static/media/doctor-performing-routine-medical-checkup 3.692fe913.jpg"
+  },
+  {
+    "revision": "e648621e455e108f5a8bf25d8e320d2a",
+    "url": "/static/media/logo_sech_white.e648621e.svg"
+  }
+]);
